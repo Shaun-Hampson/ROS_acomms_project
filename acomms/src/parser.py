2413670cd02eeffd4d009038faa6e0b1196096ab
Shaun-Hampson <srh3@hw.ac.uk>
2022-07-02 17:21:25 +0100

    Publisher in parser is not publishing to topic

--- acomms/src/parser.py
+++ acomms/src/parser.py
@@ -2,15 +2,31 @@
 
 import rospy as rp
 from std_msgs.msg import String
-from topic_tools import ShapeShifter
+from geometry_msgs.msg import PoseStamped
+#from topic_tools import ShapeShifter #TODO: cannot import ShapeShifter
+from rospy_message_converter import message_converter
 
-class parser:
-    def __init__(self):
-        rp.init_node(name = "accoms", anonymous = False)
-        rp.Subscriber()
-        
- 
-if __name__ == "__main__":
-    while not rp.is_shutdown:
-        a = parser
-        rp.Rate.sleep()
+
+rp.init_node(name = "acomms", anonymous = False)
+publisher = rp.Publisher('/acomms', String, queue_size=10)
+
+a = PoseStamped()
+#a.header = 'acomms' #TODO: throws error here as the message converter isnt hadling this string
+a.pose.position.x = 1
+a.pose.position.y = 1
+a.pose.position.z = 0
+a.pose.orientation.w = 1
+
+      
+b = message_converter.convert_ros_message_to_dictionary(a)
+
+msg = [
+    {"topic": "/test"},
+    {"type": type(a)},
+    {"msg": b}
+]
+
+c = String()
+c.data = msg
+
+publisher.publish(c)
--- acomms/src/reciever.py
+++ acomms/src/reciever.py
@@ -6,11 +6,20 @@ import json
 
 class reciever:
     def _init__(self):
-        rp.Subscriber("/acomms/out", String, self.extract)
+        rp.init_node('reciever')
+        print('setup')
+        rp.Subscriber('/acomms', String, self.extract)
+        rp.spin()
         
     def extract(self, unparsed_msg):
-        parsed_msg = json.loads(unparsed_msg)
-        data_type = parsed_msg['data_type']
-        destination_topic = parsed_msg['topic']
-        msg = parsed_msg['msg']
-        publisher = rp.Publisher(destination_topic, data_type, queue_size = None)
+        print("msg recieved")
+        #data_type = json.loads(unparsed_msg['type'])
+        #destination_topic = json.loads(unparsed_msg['topic'])
+        #msg = json.loads(unparsed_msg['msg'])
+        #publisher = rp.Publisher(destination_topic, data_type, queue_size = None)
+        
+if __name__ == "__main__":
+    while not rp.is_shutdown():
+        print('main')
+        a = reciever()
+        a._init__()
